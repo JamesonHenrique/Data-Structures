e4f53bf019028eb0d52dd2ac3d2c8f7e52b0d516
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-08-13 00:25:06 -0300

    Criar uma pilha com Linked List; Implementar funçoes basicas: Push, Pop, Display

---
+++ src/stack/list/Stack.java
@@ -0,0 +1,52 @@
+package stack.list;
+
+public class Stack {
+    public ListNode top;
+    public int length;
+    public class ListNode {
+        public int data;
+        public ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public Stack() {
+        this.top = null;
+        this.length = 0;
+    }
+    public void push(int data) {
+        ListNode newNode = new ListNode(data);
+        newNode.next = top;
+        top = newNode;
+        length++;
+
+    }
+    public void pop() {
+        if (top == null) {
+            return;
+        }
+        top = top.next;
+        length--;
+    }
+    public void display() {
+        ListNode current = top;
+        while (current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.display();
+        stack.pop();
+        stack.display();
+    }
+}
