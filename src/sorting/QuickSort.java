df7739217548f86666e9f524628af6417ba08eec
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-10-03 23:34:16 -0300

    Criar classe QuickSort; Implementar funçao para ordenar o array;

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,41 @@
+package sorting;
+
+public class QuickSort {
+    public static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+        int j = low;
+        while (j < high) {
+            if (arr[j] < pivot) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+            j++;
+        }
+        i++;
+        return i - 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        sort(arr, 0, arr.length - 1);
+        printArray(arr);
+    }
+}
