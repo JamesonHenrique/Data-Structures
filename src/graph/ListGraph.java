d43021893cc320456116e61c3b8fa29b3601c3ce
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-10-05 23:23:03 -0300

    java.lang.RuntimeException: java.net.ConnectException: Failed to connect to localhost/[0:0:0:0:0:0:0:1]:11434

--- src/graph/ListGraph.java
+++ src/graph/ListGraph.java
@@ -1,11 +1,13 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ListGraph {
     private int V;
     private int E;
-    private LinkedList[] adjList;
+    private LinkedList<Integer> []adjList;
 
 
     public ListGraph(int nodes) {
@@ -22,6 +24,55 @@ public class ListGraph {
         adjList[j].add(i);
         E++;
     }
+    public void bfs(int s) {
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+        visited[s] = true;
+        queue.offer(s);
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            System.out.print(u + " ");
+            for (int v : adjList[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    queue.offer(v);
+                }
+            }
+        }
+    }
+    public void dfs(int s) {
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(s);
+        visited[s] = true;
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            System.out.print(u + " ");
+            for (int v : adjList[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    stack.push(v);
+                }
+            }
+        }
+    }
+
+
+    public void dfsRecursive(int s) {
+        boolean[] visited = new boolean[V];
+        dfsRecursive(s, visited);
+    }
+
+    private void dfsRecursive(int s, boolean[] visited) {
+        visited[s] = true;
+        System.out.print(s + " ");
+        for (int v : adjList[s]) {
+            if (!visited[v]) {
+                dfsRecursive(v, visited);
+            }
+        }
+    }
+
 
     public void printGraph() {
         for (int i = 0; i < V; i++) {
@@ -34,12 +85,21 @@ public class ListGraph {
     }
 
     public static void main(String[] args) {
-        ListGraph graph = new ListGraph(4);
+        ListGraph graph = new ListGraph(5);
         graph.addEdge(0, 1);
         graph.addEdge(1, 2);
         graph.addEdge(2, 3);
         graph.addEdge(3, 0);
+        graph.addEdge(4, 2);
         graph.printGraph();
+        System.out.println("BFS");
+        graph.bfs(0);
+        System.out.println();
+        System.out.println("DFS");
+        graph.dfs(0);
+        System.out.println();
+        System.out.println("DFS Recursive");
+        graph.dfsRecursive(0);
     }
 
 
